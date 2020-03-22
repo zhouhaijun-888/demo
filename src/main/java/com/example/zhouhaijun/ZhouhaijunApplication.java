15549283283f783f8501da14dafae4105c0b4d95
zhouhaijun <mbohemian@163.com>
2020-03-22 17:05:09 +0800

    Initial commit

---
+++ src/main/java/com/example/zhouhaijun/ZhouhaijunApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhouhaijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhouhaijunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhouhaijunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zhouhaijun/ZhouhaijunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhouhaijun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhouhaijunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
